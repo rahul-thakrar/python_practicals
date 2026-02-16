39f0f10f7bce900f1745607664fef003e29f1761
RAHUL THAKRAR <128139246+rahul-thakrar@users.noreply.github.com>
2026-02-16 13:43:33 +0530

    Add files via upload

---
+++ 1-simpleinteres.py
@@ -0,0 +1,11 @@
+# Write a program to input p, r, n and find out interest using simple input output statement.
+
+
+p=float(input("enter a value p:"))
+r=float(input("enter a value r:"))
+n=float(input("enter a value n:"))
+
+intr=(p * r * n)/100
+
+print("simple intreset is:",intr)
+      
