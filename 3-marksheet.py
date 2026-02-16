0f1e5ab70f7c83561bc3683bf1ce6712bf75b505
RAHUL THAKRAR <128139246+rahul-thakrar@users.noreply.github.com>
2026-02-16 13:44:03 +0530

    Add files via upload

---
+++ 3-marksheet.py
@@ -0,0 +1,28 @@
+# Write a program to enter marks of 4 subjects and display result (result shall include total, percentage and grade)
+
+
+sub1 = int(input("enter your marks for Java : "))
+sub2 = int(input("enter your marks for OS : "))
+sub3 = int(input("enter your marks for RDBMS : "))
+sub4 = int(input("enter your marks for DS : "))
+
+total=sub1+sub2+sub3+sub4
+
+percentage=total/400*100
+
+print("Total Marks : ",total)
+print("percentage : ",percentage)
+
+if percentage >= 90:
+    print("grade : A")
+elif percentage >= 80:
+    print("grade : B")
+elif percentage >= 70:
+    print("grade : C")
+elif percentage >= 60:
+    print("grade : D")
+elif percentage >= 50:
+    print("grade : E")
+else:
+    print("grade : F")
+    
